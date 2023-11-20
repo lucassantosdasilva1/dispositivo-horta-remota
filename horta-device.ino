c5ec96e19050aa0604a61a36fa82f756dcb0e039
lucassantosdasilva1 <lucassantosdasilva1@hotmail.com>
2023-11-20 18:58:56 -0300

    :construction: projeto comunicando via mqttp

--- horta-device.ino
+++ horta-device.ino
@@ -4,6 +4,8 @@
 #include <ArduinoJson.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 char jsonOutput[2100];
 //=========================================== U M I D A D E  DO  S O L O =============================================
@@ -15,17 +17,33 @@ float PorcentagemUmidade;
 
 float loopUmidadeSolo() {
   Umidade = analogRead(sensorUmidadeSolo);
-  PorcentagemUmidade = map(Umidade, 1660, 4095, 100, 0);
+  if (Umidade > 4095) {
+    Umidade = 4095;
+  } else if (Umidade < 2160) {
+    Umidade = 2160;
+  }
+  
+  PorcentagemUmidade = map(Umidade, 2160, 4095, 100, 0);
+
+  if (PorcentagemUmidade > 100) {
+    PorcentagemUmidade = 100;
+  } else if (PorcentagemUmidade < 0) {
+    PorcentagemUmidade = 0;
+  }
+
+  return PorcentagemUmidade;
+}
+void statusUmidadeSolo() {
   Serial.print("Umidade do solo:");
   Serial.print(PorcentagemUmidade);
   Serial.println("%");
   Serial.print("amperagem recebida:");
   Serial.println(Umidade);
-
-  return PorcentagemUmidade;
 }
 //=========================================== T E M P E R A T U R A  DO  S O L O =============================================
 #define ONE_WIRE_BUS 25
+
+float temperaturaSolo;
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -35,38 +53,38 @@ void setupTempSolo() {
 }
 float loopTempSolo() {
   sensors.requestTemperatures();
-  float tempC = sensors.getTempCByIndex(0);
-  Serial.print("Temperatura do solo: ");
-  Serial.println(tempC);
+  temperaturaSolo = sensors.getTempCByIndex(0);
 
-  return tempC;
+  return temperaturaSolo;
+}
+void statusTempSolo() {
+  Serial.print("Temperatura do solo: ");
+  Serial.println(temperaturaSolo);
 }
 //=========================================== D H T =============================================
 #define DHTPIN 26  //Pino one está o DHT22
 
 DHT dht(26, DHT11); //Objeto que faz a leitura da temperatura e umidade
 
-float temperature = 0;
-float humidity = 0;
+float temperaturaAr = 0;
+float umidadeAr = 0;
 
 void setupDHT() {
   dht.begin();
 }
 float loopDHT() {
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
-  Serial.println("----------------------------");
+  temperaturaAr = dht.readTemperature();
+  umidadeAr = dht.readHumidity();
 
+  return temperaturaAr, umidadeAr;
+}
+void statusDHT() {
   Serial.print("Temperatura do ar: ");
-  Serial.print(temperature);
+  Serial.print(temperaturaAr);
   Serial.println(" C");
   Serial.print("Umidade do ar: ");
-  Serial.print(humidity);
+  Serial.print(umidadeAr);
   Serial.println("%");
-  
-  Serial.println("----------------------------");
-
-  return temperature, humidity;
 }
 
 //=========================================== W I F I =============================================
@@ -122,23 +140,109 @@ void postHTTP() {
   }
 }
 
+//=========================================== M Q T T  =============================================
+const char* mqtt_server = "maqiatto.com";
+const char* mqtt_user = "lucashouse0@gmail.com";
+const char* mqtt_password = "hortas";
+const char* mqtt_topic = "lucashouse0@gmail.com/tempsolo";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(256)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  // if ((char)payload[0] == '1') {
+  //   digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  //   // but actually the LED is on; this is because
+  //   // it is active low on the ESP-01)
+  // } else {
+  //   digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  // }
+
+}
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(mqtt_topic, "hellosss world");
+      // ... and resubscribe
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void setupMQTT() {
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+void loopMQTT() {
+  float tempSolo = loopTempSolo();
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    // snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    snprintf (msg, MSG_BUFFER_SIZE, "%f, %f, %f, %f ", temperaturaSolo, PorcentagemUmidade, temperaturaAr, umidadeAr);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(mqtt_topic, msg);
+  }
+}
+//========================================================================================
+
+
 void setup() {
   delay(100);
   setupWIFI();
   setupDHT();
   setupTempSolo();
+  setupMQTT();
   delay(2000);
 
   
   Serial.begin(115200);
 }
 
+
+
 void loop() {
   
   loopDHT();
   loopTempSolo();
   loopUmidadeSolo();
+  statusUmidadeSolo();
   Serial.println("----------------------------");
+  loopMQTT();
   //postHTTP();
   delay(10000);
 
